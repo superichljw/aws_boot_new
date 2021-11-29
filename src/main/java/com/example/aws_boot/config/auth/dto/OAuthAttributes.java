f32419d20185254e7b469699d9e3496da3e2ba97
leejw <leejw@arainhr.com>
2021-11-29 19:38:50 +0900

    네이버 로그인 기능 추가 - application-oauth 파일을 ignore 처리해서 실제로 집에서 할때는 네이버 로그인 설정값 따로 입력한뒤 진행할것

--- src/main/java/com/example/aws_boot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/example/aws_boot/config/auth/dto/OAuthAttributes.java
@@ -25,7 +25,11 @@ public class OAuthAttributes {
     }
     // of() : OAuth2User 에서 반환하는 사용자 정보는 Map 타입
     // 그렇기 때문에 값 하나하나를 변환해야 한다
+    // 네이버 내용 추가
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String,Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id",attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -36,6 +40,18 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String,Object> attributes){
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
